4c01391f421dd930500acef5ce662cf579307faa
rumnast <rumnast@mail.ru>
2021-03-01 23:55:52 +0300

    homework lesson 5

--- scripts/script_lesson5.js
+++ scripts/script_lesson5.js
@@ -11,6 +11,7 @@ const app = new Vue({
     methods: {
         toggleCart() {
             this.showCart = !this.showCart;
+            //console.log(this.showCart);          
         },
         addToCart(item) {
             let existant = false;
@@ -28,15 +29,33 @@ const app = new Vue({
                 }
             }
             if (!existant) {
-                this.cartItems.push({ ...item, quantity: 1, sum: this.cartItems.price, img: "" });
-
-
+                //console.log(this.cartItems);
+                this.cartItems.push({ ...item, quantity: 1, sum: item.price, img: `img/${item.id_product}.jpg` });
+                //console.log(this.cartItems);
                 //console.log(existant);
                 //console.log("laalalalal");
             }
-            console.log(this.cartItems);
+            //console.log(this.cartItems);
+
         },
-        removeFromCart(id) { },
+        removeFromCart(item) {
+            //console.log(this.cartItems)
+            for (goodsItem of this.cartItems) {
+                if (goodsItem.id_product == item.id_product) {
+                    if (goodsItem.quantity > 1) {
+                        //console.log(goodsItem.quantity)
+                        goodsItem.quantity -= 1;
+                        let sum = goodsItem.quantity * goodsItem.price;
+                        goodsItem.sum = sum;
+                    } else {
+                        const index = this.cartItems.indexOf(goodsItem);
+                        this.cartItems.splice(index, 1);
+
+                        //this.cartItems.pop({ ...item, quantity: 1, sum: this.cartItems.price, img: "" });
+                    }
+                }
+            }
+        },
         filterGoods() {
             if (!this.goods.length) this.filteredGoods = [];
             if (!this.searchGoods) this.filteredGoods = this.goods;
@@ -47,17 +66,33 @@ const app = new Vue({
                 .then(r => r.json())
                 .then(r => {
                     this.goods = r;
-                    console.log(r);
+                    //console.log(r);
                     this.filteredGoods = this.goods;
                 })
+                .then(r => {
+                    let image = document.querySelectorAll(".goodsImg");
+                    //console.log(image);
+                    for (let i = 0; i < image.length; i++) {
+                        //let im = `<img src="img/${this.goods[i].id_product}.jpg" alt="photo">`
+                        //console.log(this.goods[i].id_product);
+                        image[i].innerHTML = `<img src="img/${this.goods[i].id_product}.jpg" alt="photo">`;
+                        //console.log(image[i]);
+                    }
+                })
+
+
                 .catch(e => {
                     this.errorMessage = e;
                 });
         },
+
+
     },
     mounted() {
         this.getGoods();
     },
 });
+
+
 
 
